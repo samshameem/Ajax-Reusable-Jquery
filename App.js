2e86aa5a0d5521b6c8efc5ca15f53c28404fec91
Shameem Ahmed Mulla <shameem.sam@gmail.com>
2020-05-07 18:16:58 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,69 +2,140 @@
 	"use strict";
 
 	var App = function () {
-		var o = this; // Create reference to this instance
+		// Create reference to this instance
+		var o = this;
+		// Initialize app when document is ready
 		$(document).ready(function () {
 			o.initialize();
-		}); // Initialize app when document is ready
+		});
 
 	};
 	var p = App.prototype;
 
+	// Private
+	p._modified = false;
 	// =========================================================================
 	// INIT
 	// =========================================================================
 
 	p.initialize = function () {
-		// Init events
-		this._enableEvents();
+		this._initEvents();
 	};
 
 	// =========================================================================
-	// EVENTS
+	// Initialize Events
 	// =========================================================================
 
-	// events
-	p._enableEvents = function () {
+	p._initEvents = function () {
 		var o = this;
-		var formData = {};
 
-		o._request({
-			ajax:{
-				url: 'php/initiate/check',
-				data: formData
-			},
-			function(data) {
-				console.log(data);
-			}
+	};
+
+	p._alertModified = function(){
+		var o = this;
+
+		o._modified = false;
+		window.onbeforeunload = s => o._modified ? "" : null;
+		$('input').on('change', function(){
+			o._modified = true;
 		});
-	};
+	}
 
 	// =========================================================================
-	// AJAX REQUEST
+	// REQUEST
 	// =========================================================================
-
-	p._request = function (source) {
+	p._request = function (e, param, options, callback) {
+		// var btn = $(e.currentTarget), o = this;
 		var o = this;
-		var ajax = {
-			type: 'POST',
+		var defaults = {
+			msg: 'Processing...',
+			cancelAjax: false,
+			type: 'GET',
 			url: '',
-			dataType: 'json',
-			data: {}
+			target: false,
+			cache: false,
+			loading: false,
+			dataArray: '',
 		}
 
-		ajax = $.extend( true,  ajax, source.ajax );
+		options.url = base_url+options.url;
 
-		$.ajax( $.extend( true, {
+		options = $.extend({}, defaults, options);
+
+		o._ajaxRequest = $.ajax( $.extend( true, {
+			type: options.type,
+			url: options.url,
+			dataType: 'json',
+			data: options.dataArray,
 			success : function(data,status,jqXHR){
-				source.function success(data);
+				if (options.loading) $('#loading').hide();
+				if(data && data.response === true){
+					if (options.cache) {
+						o.localCache.set(JSON.stringify(options.dataArray), data);
+					};
+					callback(data);
+				}else if(data && data.response === false){
+					alert(data.message);
+				}else{
+					alert('Error: There was an error, Please try again.');
+				}
+			},
+			error : function (jqXHR,status,data){
+				if (options.loading) $('#loading').hide();;
+				alert('Error: There was an error, Please try again.');
+			},
+			beforeSend : function () {
+				if (options.loading) $('#loading').show();
+				if(o._ajaxRequest != null && options.cancelAjax) {
+					o._ajaxRequest.abort();
+				}
+				if (options.cache) {
+					if (o.localCache.exist(JSON.stringify(options.dataArray))) {
+						callback(o.localCache.get(JSON.stringify(options.dataArray)));
+						if (options.loading) $('#loading').hide();
+						return false;
+					}
+					return true;
+				};
+			},
+			complete : function () {
+				if (options.loading) $('#loading').hide();
+				o._modified = false;
 			}
-		}, ajax ));
+		}, param ));
 	};
+
+	// =========================================================================
+	// CACHE
+	// =========================================================================
+
+	p.localCache = {
+		data: {},
+		remove: function (url) {
+			delete p.localCache.data[url];
+		},
+		exist: function (url) {
+			return p.localCache.data.hasOwnProperty(url) && p.localCache.data[url] !== null;
+		},
+		get: function (url) {
+			return p.localCache.data[url];
+		},
+		set: function (url, cachedData, callback) {
+			p.localCache.remove(url);
+			p.localCache.data[url] = cachedData;
+			if ($.isFunction(callback)) callback(cachedData);
+		}
+	};
+
+	Object.defineProperty(p.localCache, "data", {
+		writable: false
+	});
 
 	// =========================================================================
 	// DEFINE NAMESPACE
 	// =========================================================================
 
-	window.samshameem = window.samshameem || {};
-	window.samshameem.App = new App;
+	window.ion = window.ion || {};
+	window.ion.App = new App;
 }(jQuery)); // pass in (jQuery):
+//List User Script Ends Here
